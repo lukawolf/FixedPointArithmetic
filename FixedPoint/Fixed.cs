a8996faff7312e87a36417a57add45bf0726e599
Lukas Cakl <cakl.lukas@gmail.com>
2019-03-23 15:53:25 +0100

    Change internal attributes to int, only cast to long when needed for operations which exceed 32bits

--- FixedPoint/Fixed.cs
+++ FixedPoint/Fixed.cs
@@ -7,18 +7,18 @@ namespace Cuni.Arithmetics.FixedPoint
 {
     public class Fixed<Q> where Q:IPrecision
     {
-        private long raw;
-        private static long one; 
-        private static int beforeDot;
-        private static int afterDot;
-        private static Regex precisionRegex = new Regex(@"Q([1-9][0-9]*)_([1-9][0-9]*)", RegexOptions.Compiled);
+        private int raw;
+        private static readonly int one; 
+        private static readonly int beforeDot;
+        private static readonly int afterDot;
+        private static readonly Regex precisionRegex = new Regex(@"Q([1-9][0-9]*)_([1-9][0-9]*)", RegexOptions.Compiled);
         static Fixed()
         {
             var match = precisionRegex.Match(typeof(Q).Name);
             if(!match.Success) throw new ArgumentException("Precision needs to follow Q<number>_<number> format");
             beforeDot = int.Parse(match.Groups[1].Value);
             afterDot = int.Parse(match.Groups[2].Value);
-            one = ((long)1) << afterDot;
+            one = 1 << afterDot;
             if (beforeDot + afterDot != 32) throw new ArgumentException("Precision needs to add up to 32 bits");
         }
         public Fixed(int value){
@@ -42,15 +42,14 @@ namespace Cuni.Arithmetics.FixedPoint
         public Fixed<Q> Multiply(Fixed<Q> value)
         {
             var toReturn = new Fixed<Q>(0);
-            toReturn.raw = (raw * value.raw) >> afterDot;
-            toReturn.raw = (toReturn.raw << 32) >> 32; //To simulate overflow as specified in email
+            toReturn.raw = (int)(((long)raw * (long)value.raw) >> afterDot);
             return toReturn;
         }
 
         public Fixed<Q> Divide(Fixed<Q> value)
         {
             var toReturn = new Fixed<Q>(0);
-            toReturn.raw = (raw << afterDot) / value.raw;
+            toReturn.raw = (int)(((long)raw << afterDot) / (long)value.raw);
             return toReturn;
         }
 
